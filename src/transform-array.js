bde15a951e176a191c7db53d1f2336bdf12bec3f
Kanstantsin Kulak <kkulak24@gmail.com>
2020-02-26 22:04:50 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,33 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    } else {
+        var res = [];
+        for (let i = 0; i < arr.length; i++) {
+            var current = arr[i];
+            var type = typeof current;
+            switch (current) {
+                case "--discard-next":
+                    i++;
+                    continue;
+                case "--discard-prev":
+                    res.pop();
+                    break;
+                case "--double-next":
+                    if (arr[i + 1] != undefined) {
+                        res.push(arr[i + 1]);
+                    }
+                    break;
+                case "--double-prev":
+                    if (arr.length != 0 && arr[i - 1] != undefined) {
+                        res.push(arr[i - 1]);
+                    }
+                    break;
+                default:
+                    res.push(current)
+                    break;
+            }
+        }
+        return res;
+    }
 };
