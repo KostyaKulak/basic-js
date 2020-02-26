e0c003d84c54c69a79ced015b67d07eb46410d97
Kanstantsin Kulak <kkulak24@gmail.com>
2020-02-26 14:04:49 +0300

    feat: implement repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,19 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    var res = [];
+    var additions = [];
+    var k = options.additionRepeatTimes == undefined ? 1 : options.additionRepeatTimes;
+    if ("addition" in options) {
+        for (let i = 0; i < k; i++) {
+            var s = options.addition == null ? "null" : options.addition;
+            additions.push(s);
+        }
+    }
+    var addition = additions.join(options.additionSeparator);
+    var n = options.repeatTimes == undefined ? 1 : options.repeatTimes;
+    for (let i = 0; i < n; i++) {
+        var s = str == null ? "null" : str;
+        res.push(s + addition);
+    }
+    var separator = options.separator == undefined ? "+" : options.separator;
+    return res.join(separator);
+}
