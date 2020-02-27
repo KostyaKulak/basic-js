9edaca2d1e3cbbfdc514cf5e1eb4c837b41d783b
Kanstantsin Kulak <kkulak24@gmail.com>
2020-02-27 13:48:34 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,70 @@
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct) {
+        this.direct = direct == undefined ? true : direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        this.verify(message, key);
+        var n = message.replace(/[^a-zA-Z]/g, '').length;
+        while (key.length < n) {
+            key += key;
+        }
+        if (key.length > n) {
+            key = key.substring(0, n);
+        }
+        var i = 0, j = 0;
+        var res = "";
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        while (i < message.length) {
+            if (ALPHABET.includes(message[i])) {
+                var index = (ALPHABET.indexOf(message[i]) + ALPHABET.indexOf(key[i - j])) % ALPHABET.length;
+                var temp = ALPHABET[index];
+                res += temp;
+            }
+            else {
+                res += message[i];
+                j++;
+            }
+            i++;
+        }
+        return this.direct ? res : res.split("").reverse().join("");
+    }
+
+    decrypt(message, key) {
+        this.verify(message, key);
+        var n = message.replace(/[^a-zA-Z]/g, '').length;
+        while (key.length < n) {
+            key += key;
+        }
+        if (key.length > n) {
+            key = key.substring(0, n);
+        }
+        var i = 0, j = 0;
+        var res = "";
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        while (i < message.length) {
+            if (ALPHABET.includes(message[i])) {
+                var index = (ALPHABET.indexOf(message[i]) - ALPHABET.indexOf(key[i - j]) + ALPHABET.length) % ALPHABET.length;
+                var temp = ALPHABET[index];
+                res += temp;
+            }
+            else {
+                res += message[i];
+                j++;
+            }
+            i++;
+        }
+        return this.direct ? res : res.split("").reverse().join("");
+    }
+
+    verify(message, key) {
+        if (message == undefined || key == undefined || typeof message != "string" || typeof message != "string") {
+            throw new Error();
+        }
     }
 }
 
